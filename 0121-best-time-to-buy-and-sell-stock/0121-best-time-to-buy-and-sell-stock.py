063e76ae4fa386c55139d24ebe6e2c9ad82bdac2
eoxkxid <eoxkxid@gmail.com>
2026-06-01 20:18:25 +0900

    Time: 41 ms (69.67%), Space: 28.6 MB (42.47%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,23 +1,14 @@
+from typing import List
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        min_price = float('inf')
+        min_price = prices[0]
         max_profit = 0
 
         for price in prices:
             if price < min_price:
-                min_price = price  # Update the lowest price so far
-            elif price - min_price > max_profit:
-                max_profit = price - min_price  # Update max profit if current profit is higher
+                min_price = price
+            else:
+                max_profit = max(max_profit, price - min_price)
 
         return max_profit
-
-'''
-float('inf') means infinity.
-It's a built-in way to represent a number that's larger than any other number.
-There is also float('-inf') for negative infinity.
-
-Note:
-In the line `if price < min_price:`
-This means that if the current price is smaller than the lowest price we've seen so far, we update it.
-If the price is higher, we check if selling now would give us a better profit.
-'''
