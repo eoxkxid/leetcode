0ea4048e090ae012a6cf68eb032baecb55d7d96a
eoxkxid <eoxkxid@gmail.com>
2026-06-19 14:08:26 +0900

    Time: 43 ms (76.75%), Space: 19.2 MB (89.85%) - LeetHub

--- 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.py
+++ 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.py
@@ -1,51 +1,44 @@
 class Codec:
     def __init__(self):
+        # 짧은 URL의 공통 앞부분
         self.base_url = "http://tinyurl.com/"
-        self.chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        
-        self.long_to_code = {}
-        self.code_to_long = {}
+
+        # shortUrl -> longUrl
+        # decode할 때 사용
+        self.short_to_long = {}
+
+        # longUrl -> shortUrl
+        # 같은 longUrl이 다시 들어왔는지 확인할 때 사용
+        self.long_to_short = {}
+
+        # 새로운 shortUrl 번호를 만들기 위한 카운터
         self.counter = 0
-
-    def _to_base62(self, num: int) -> str:
-        # 0도 유효한 코드로 처리
-        if num == 0:
-            return self.chars[0]
-
-        code = []
-
-        while num > 0:
-            remainder = num % 62
-            code.append(self.chars[remainder])
-            num //= 62
-
-        # 나머지부터 구했기 때문에 뒤집어야 정상적인 Base62 표현이 된다
-        return ''.join(reversed(code))
 
     def encode(self, longUrl: str) -> str:
         """
         긴 URL을 짧은 URL로 인코딩한다.
         """
-        # 이미 인코딩한 URL이면 기존 코드를 재사용
-        if longUrl in self.long_to_code:
-            code = self.long_to_code[longUrl]
-            return self.base_url + code
+        # 이미 인코딩한 긴 URL이면 기존 shortUrl을 그대로 반환
+        if longUrl in self.long_to_short:
+            return self.long_to_short[longUrl]
 
-        # 새로운 URL이면 새로운 코드 생성
-        code = self._to_base62(self.counter)
+        # 처음 보는 긴 URL이면 새로운 shortUrl 생성
+        shortUrl = self.base_url + str(self.counter)
+
+        # 다음 URL을 위해 counter 증가
         self.counter += 1
 
-        self.long_to_code[longUrl] = code
-        self.code_to_long[code] = longUrl
+        # 양방향 매핑 저장
+        self.short_to_long[shortUrl] = longUrl
+        self.long_to_short[longUrl] = shortUrl
 
-        return self.base_url + code
+        return shortUrl
 
     def decode(self, shortUrl: str) -> str:
         """
         짧은 URL을 원래 URL로 디코딩한다.
         """
-        code = shortUrl.split("/")[-1]
-        return self.code_to_long[code]
+        return self.short_to_long[shortUrl]
         
 
 # Your Codec object will be instantiated and called as such:
