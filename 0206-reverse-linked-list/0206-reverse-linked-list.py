8690e60bc2d80ac1ceef72e797dc9fd344cb0ab4
eoxkxid <eoxkxid@gmail.com>
2026-07-17 22:48:30 +0900

    Time: 0 ms (100%), Space: 20.5 MB (65.1%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(
+        self,
+        head: Optional[ListNode]
+    ) -> Optional[ListNode]:
+
+        previous = None
+        current = head
+
+        while current is not None:
+            # 현재 연결을 바꾸기 전에 다음 노드를 저장
+            next_node = current.next
+
+            # 현재 노드가 이전 노드를 가리키도록 방향 변경
+            current.next = previous
+
+            # 두 포인터를 한 칸씩 앞으로 이동
+            previous = current
+            current = next_node
+
+        # 반복 종료 후 previous가 새로운 head
+        return previous
+        
