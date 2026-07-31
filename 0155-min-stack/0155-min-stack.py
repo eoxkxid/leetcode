19fa6f266151f4db981bdd9c6aed29cf7dbcf382
eoxkxid <eoxkxid@gmail.com>
2026-07-31 17:56:38 +0900

    Time: 111 ms (28.64%), Space: 32.9 MB (10.37%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -4,23 +4,23 @@ class MinStack:
         # 각 원소를 (실제 값, 현재까지의 최솟값) 형태로 저장
         self.stack = []
 
-    def push(self, val: int) -> None:
+    def push(self, value: int) -> None:
         if not self.stack:
             # 첫 번째 원소는 그 자체가 최솟값
-            current_min = val
+            current_min = value
         else:
             # 기존 최솟값과 새로운 값을 비교
             previous_min = self.stack[-1][1]
-            current_min = min(val, previous_min)
+            current_min = min(value, previous_min)
 
-        self.stack.append((val, current_min))
+        self.stack.append((value, current_min))        
 
     def pop(self) -> None:
         self.stack.pop()
 
     def top(self) -> int:
         # 튜플의 첫 번째 값이 실제 스택 원소
-        return self.stack[-1][0]
+        return self.stack[-1][0]      
 
     def getMin(self) -> int:
         # 튜플의 두 번째 값이 현재 스택의 최솟값
