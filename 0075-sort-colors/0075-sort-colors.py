20678b30403bb812ac39ab71634285dacaf117a9
eoxkxid <eoxkxid@gmail.com>
2026-08-20 18:23:17 +0900

    Time: 0 ms (100%), Space: 19.4 MB (22.75%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,13 +3,14 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+
         low = 0
         mid = 0
         high = len(nums) - 1
 
         while mid <= high:
             if nums[mid] == 0:
-                # 0을 왼쪽 영역으로 이동
+                # 0을 왼쪽으로 이동
                 nums[low], nums[mid] = nums[mid], nums[low]
                 low += 1
                 mid += 1
@@ -17,11 +18,11 @@ class Solution:
             elif nums[mid] == 1:
                 # 1은 이미 올바른 가운데 영역에 있음
                 mid += 1
-
-            else:  # nums[mid] == 2
-                # 2를 오른쪽 영역으로 이동
+            
+            else: # nums[mid] == 2
+                # 2를 오른쪽으로 이동
                 nums[mid], nums[high] = nums[high], nums[mid]
                 high -= 1
 
                 # high에서 가져온 값은 아직 확인하지 않았으므로
-                # mid는 증가시키지 않는다.     
+                # mid는 증가시키지 않는다.                
