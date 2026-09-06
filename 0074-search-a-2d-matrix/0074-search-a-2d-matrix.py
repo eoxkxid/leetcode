fc5a49fc6ae0d03a11a08724236ac0976094b30d
eoxkxid <eoxkxid@gmail.com>
2026-09-06 21:51:02 +0900

    Time: 0 ms (100%), Space: 19.4 MB (79.42%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -0,0 +1,25 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        rows = len(matrix)
+        cols = len(matrix[0])
+
+        left = 0
+        right = rows * cols - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            # 1차원 인덱스를 2차원 좌표로 변환
+            row = mid // cols
+            col = mid % cols
+
+            value = matrix[row][col]
+
+            if value == target:
+                return True
+            elif value < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return False
