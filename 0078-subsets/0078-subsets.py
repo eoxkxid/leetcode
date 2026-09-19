31420f6ef84afbd0762f7ab2fb272b32c0740499
eoxkxid <eoxkxid@gmail.com>
2026-09-19 09:27:34 +0900

    Time: 4 ms (2.84%), Space: 19.4 MB (69.06%) - LeetHub

---
+++ 0078-subsets/0078-subsets.py
@@ -0,0 +1,23 @@
+class Solution:
+    def subsets(self, nums: list[int]) -> list[list[int]]:
+        result = []
+        path =[]
+
+        def backtrack(start):
+            # 현재 path 자체가 하나의 부분집합
+            result.append(path.copy())
+
+            # start 이후의 원소를 하나씩 선택
+            for i in range(start, len(nums)):
+                # 선택
+                path.append(nums[i])
+
+                # 다음 원소부터 탐색
+                backtrack(i + 1)
+
+                # 선택 취소
+                path.pop()
+
+        backtrack(0)
+
+        return result
